4b35fa42348f346ecd6743d52e586582f119b5a2
Cha-xeo <arnaud.lalande@epitech.eu>
2024-01-03 16:33:42 +0100

    EndMenu

--- Assets/Scripts/EndCanvasController.cs
+++ Assets/Scripts/EndCanvasController.cs
@@ -5,14 +5,29 @@ namespace Assets.Scripts
 {
     public class EndCanvasController : MonoBehaviour
     {
-
+        bool _canInteract;
         // Update is called once per frame
         void Update()
         {
-            if (Input.anyKeyDown)
+            if (_canInteract && Input.anyKeyDown)
             {
                 gameObject.SetActive(false);
             }
+        }
+
+        private void OnEnable()
+        {
+            Invoke(nameof(SetInteractibility), 1f);
+        }
+
+        private void OnDisable()
+        {
+            _canInteract = false;
+        }
+
+        void SetInteractibility()
+        {
+            _canInteract = true;
         }
     }
 }
